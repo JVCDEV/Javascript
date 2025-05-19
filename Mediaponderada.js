16cc9cfdafdc7e8b7d3ff9c095e221213af4bc41
unknown <joaocalabria97@gmail.com>
2025-05-19 20:14:41 -0300

    Adiciona cálculo da média ponderada

---
+++ Mediaponderada.js
@@ -0,0 +1,15 @@
+// Declaração das notas
+let n1 = 7.5;
+let n2 = 8.0;
+let n3 = 9.0;
+
+// Declaração dos pesos
+let p1 = 2;
+let p2 = 3;
+let p3 = 5;
+
+// Cálculo da média ponderada
+let media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+// Exibe o resultado no console
+console.log("Média ponderada:", media);
